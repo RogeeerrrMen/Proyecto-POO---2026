13f31e2de4e242aea87b4b87565dbaf6406cf87f
RogeeerrrMen <men26786@uvg.edu.gt>
2026-09-25 08:57:48 -0600

    implementacion de la dependencia Thymeleaf y primera pagina de Inicio

--- src/main/java/edu/uvg/gestionpedidos/controller/HomeController.java
+++ src/main/java/edu/uvg/gestionpedidos/controller/HomeController.java
@@ -1,13 +1,13 @@
 package edu.uvg.gestionpedidos.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String inicio() {
-        return "Sistema de Gestion de Pedidos";
+        return "home";
     }
 }
